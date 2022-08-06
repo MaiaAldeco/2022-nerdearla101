4ecfe905426022cb6ed06b0d229351f3af86eeb4
MaiaAldeco <maiaaldeco@gmail.com>
2022-08-05 22:20:08 -0300

    milestone 1

---
+++ api-catalog/src/test/java/com/twa/apicatalog/ApiCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twa.apicatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
